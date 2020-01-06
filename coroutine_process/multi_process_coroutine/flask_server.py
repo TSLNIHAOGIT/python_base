a193c69ba7abad014b077415f324478cbba55d93
taoshilai <taoshilai@x3platform.com>
2020-01-06 14:37:16 -0700

    update flask count_uid

--- coroutine_process/multi_process_coroutine/flask_server.py
+++ coroutine_process/multi_process_coroutine/flask_server.py
@@ -2,11 +2,13 @@ from flask import Flask
 import time
 
 app = Flask(__name__)
-
+count_uid=0
 @app.route('/')
 def index():
+    global count_uid
     time.sleep(3)
-    return 'Hello!'
+    count_uid=count_uid+1
+    return 'Hello!'+str(count_uid)
 
 if __name__ == '__main__':
     app.run(threaded=True)
